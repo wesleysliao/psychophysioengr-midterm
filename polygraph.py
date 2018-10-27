d3af583a0be6d135559209ed329e8ec7ad021f01
wesleysliao <wesleysliao@gmail.com>
2018-10-27 12:38:29 -0400

    Generalized subject parameter passing, seperated data file loading, removed fft calculation for now

--- polygraph.py
+++ polygraph.py
@@ -23,6 +23,8 @@ class SensorData:
     def __init__(self, name, rawdata):
         self.name = name
         self.rawdata = rawdata
+        
+        print("        - Cleaning data")
         self.cleandata = self.process(self.rawdata)
         
     def process(self, data):
@@ -39,12 +41,15 @@ class PeriodicSensorData(SensorData):
     def __init__(self, name, rawdata):
         super().__init__(name, rawdata)
 
-        self.fft = fftpack.fftshift(fftpack.fft(self.cleandata.values))
-        bins = len(self.fft)
-        nyquist = self.cleandata.samplerate_Hz/2
-        freqmax = nyquist/2
-        self.freqrange = np.linspace(-freqmax, freqmax, bins)
+#        print("        - Processing FFT")
+#        self.fft = fftpack.fftshift(fftpack.rfft(self.cleandata.values))
+#        bins = len(self.fft)
+#        nyquist = self.cleandata.samplerate_Hz/2
+#        freqmax = nyquist/2
+#        self.freqrange = np.linspace(-freqmax, freqmax, bins)
         
+        
+        print("        - Finding Peaks")
         self.peaks = self.find_peaks(self.cleandata)
         
     def plot(self):
@@ -52,10 +57,10 @@ class PeriodicSensorData(SensorData):
         plt.plot(self.peaks.times, self.peaks.values, linestyle="None", marker="^", markersize=10)
         plt.legend(["Raw Data", "Cleaned Data", "Peaks"])
     
-    def plotfft(self):
-        plt.plot(self.freqrange, self.fft)
-        plt.xlabel("Frequency (Hz)")
-        plt.ylabel(str(self.cleandata.units))
+#    def plotfft(self):
+#        plt.plot(self.freqrange, self.fft)
+#        plt.xlabel("Frequency (Hz)")
+#        plt.ylabel(str(self.cleandata.units))
     
     def find_peaks(self,data):
         peaks, properties = signal.find_peaks(data.values)
@@ -65,7 +70,8 @@ class PeriodicSensorData(SensorData):
             return None
 
 class BloodPressure(PeriodicSensorData):
-    def __init__(self, data, times):
+    def __init__(self, data, times, peakinterval=500):
+        self.peakinterval = peakinterval
         rawdata = Timeseries(data, timestamps=times, samplerate_Hz=1000, units="mmHg")
         super().__init__("Blood Pressure", rawdata)
     
@@ -79,7 +85,7 @@ class BloodPressure(PeriodicSensorData):
         return Timeseries(filtereddata, timestamps=data.times, samplerate_Hz=data.samplerate_Hz, units=data.units)
     
     def find_peaks(self,data):
-        peaks, properties = signal.find_peaks(data.values, distance=500, height=20)
+        peaks, properties = signal.find_peaks(data.values, distance=self.peakinterval, height=20)
         if len(peaks)>0:
             return Timeseries(data.values[peaks], timestamps=data.times[peaks], samplerate_Hz=None)
         else:
@@ -124,17 +130,21 @@ class Test():
         pass
 
 class Trial:
-    def __init__(self, subjectname, datafilename, eventfilename):
-        self.subjectname = subjectname
-        self.rawdata = np.genfromtxt(datafilename, delimiter='	', skip_header=17)
+    def __init__(self, subjectname, rawdata, param):
         
-        rawevents = np.loadtxt(eventfilename, dtype=str, delimiter=',', skiprows=2)
+        print("+ Creating "+subjectname)
+        self.subjectname = subjectname
+                
+        print("    - Reading event data from file")        
+        rawevents = np.loadtxt(param["eventfile"], dtype=str, delimiter=',', skiprows=2)
+        
+        print("    - Processing event data")
         self.events = dict()
         for i in range(len(rawevents)):
             self.events[rawevents[i, 1]]=rawevents[i,0].astype(float)
         
-        times = (self.rawdata[:,0]*60)
-        bp_data = self.rawdata[:,1]
+        times = (rawdata[:,0]*60)
+        bp_data = rawdata[:,1]
 #        rsp_data = self.rawdata[:,2]
 #        ppg_data = self.rawdata[:,3]
 #        mbp_data = self.rawdata[:,4]
@@ -144,7 +154,9 @@ class Trial:
         self.sensors = dict()
         self.parameters = dict()
         
-        bp = BloodPressure(bp_data, times)        
+        
+        print("    - Processing Blood Pressure data")
+        bp = BloodPressure(bp_data, times, param["bp_peakdist"])        
         self.sensors[bp.name] = bp
 #        
 #        rsp = RSP(rsp_data, times)        
@@ -163,18 +175,54 @@ class Trial:
 #        self.sensors[eda.name] = eda
 
 
-def load():
-    names = ["Subject A","Subject B","Subject C","Subject D","Subject E","Subject F","Subject G"]
-    datafiles = ["Subject A.txt","Subject B.txt","Subject C.txt","Subject D.txt","Subject E.txt","Subject F.txt","Subject G.txt"]
-    eventfiles = ["SubjectA_events.csv","SubjectB_events.csv","SubjectC_events.csv","SubjectD_events.csv","SubjectE_events.csv","SubjectF_events.csv","SubjectG_events.csv"]
-    
+def load_data(params):
+    print("+ Loading trial data")
+    rawdata = dict()
+    for subject in iter(params):
+        print("    - loading "+subject)
+        rawdata[subject] = np.genfromtxt(params[subject]["datafile"], delimiter='	', skip_header=17)
+    return rawdata
+
+def generate_subjects(params, rawdata):
     subjects = dict()
-    for i in range(len(names)):
-        subjects[names[i]] = Trial(names[i], "data/"+datafiles[i], "events/"+eventfiles[i])
+    for subject in iter(params):
+        subjects[subject] = Trial(subject, rawdata[subject], params[subject])
     return subjects
 
 
-subjects = load()
-for subject in subjects:
+
+params = {"Subject A": {"datafile":     "data/Subject A.txt",
+                            "eventfile":    "events/SubjectA_events.csv",
+                            "bp_peakdist":  500},
+
+              "Subject B": {"datafile":     "data/Subject B.txt",
+                            "eventfile":    "events/SubjectB_events.csv",
+                            "bp_peakdist":  300},
+                            
+              "Subject C": {"datafile":     "data/Subject C.txt",
+                            "eventfile":    "events/SubjectC_events.csv",
+                            "bp_peakdist":  500},
+                            
+              "Subject D": {"datafile":     "data/Subject D.txt",
+                            "eventfile":    "events/SubjectD_events.csv",
+                            "bp_peakdist":  500},
+                            
+              "Subject E": {"datafile":     "data/Subject E.txt",
+                            "eventfile":    "events/SubjectE_events.csv",
+                            "bp_peakdist":  500},
+                            
+              "Subject F": {"datafile":     "data/Subject F.txt",
+                            "eventfile":    "events/SubjectF_events.csv",
+                            "bp_peakdist":  500},
+                            
+              "Subject G": {"datafile":     "data/Subject G.txt",
+                            "eventfile":    "events/SubjectG_events.csv",
+                            "bp_peakdist":  500}
+             }
+
+
+rawdata = load_data(params)
+subjects = generate_subjects(params, rawdata)
+for subject in subjects.values():
     plt.figure()
     subject.sensors["Blood Pressure"].plot()
